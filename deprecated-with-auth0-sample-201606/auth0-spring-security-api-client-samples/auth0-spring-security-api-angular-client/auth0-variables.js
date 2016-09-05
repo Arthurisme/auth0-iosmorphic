ce504716f86c26e80b7e08ffc8a0b38f757c34be
Arthurisme <is@live.ca>
2016-09-05 08:45:10 -0400

    change to new sample based

---
+++ deprecated-with-auth0-sample-201606/auth0-spring-security-api-client-samples/auth0-spring-security-api-angular-client/auth0-variables.js
@@ -0,0 +1,5 @@
+var AUTH0_DOMAIN='arthurisme.auth0.com';
+var AUTH0_CLIENT_ID='WIcfe2CWGUmcmJwYEfCk763nXyGLOFM6';
+var API_SERVER_URL='http://localhost:3001';  // default server url for Java Spring Security API sample
+var DELEGATION_ENABLED=true;
+var API_SERVER_CLIENT_ID='WIcfe2CWGUmcmJwYEfCk763nXyGLOFM6';  // set to '' if DELEGATION_ENABLED=false
