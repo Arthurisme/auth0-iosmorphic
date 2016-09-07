220d3f80a4d345a015a238d9e49cd0a48fbc834f
Arthurisme <is@live.ca>
2016-09-07 10:59:55 -0400

    nativescript auth0 login ok

---
+++ Mobile/Groceries-auth0/sample-Groceries/app/auth0testpage/auth0testpage.component.ts
@@ -0,0 +1,76 @@
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { Page } from "ui/page";
+
+
+import { alert, setHintColor, LoginService, User } from "../shared";
+var auth0 = require("nativescript-auth0");
+var application = require("application");
+import * as appSettings from "application-settings";
+
+
+
+
+
+@Component({
+  selector: "gr-auth0testpage",
+  templateUrl: "auth0testpage/auth0testpage.component.html",
+  styleUrls: ["auth0testpage/auth0testpage-common.css", "auth0testpage/auth0testpage.component.css"],
+})
+export class Auth0testpageComponent   {
+
+
+
+
+
+
+  constructor(private router: Router
+ ) {
+
+  }
+
+
+
+ 
+
+    public loginauth0() {
+        console.log("TTTTT");
+
+        auth0.show().then( (args) =>{
+            console.log(args.profile);
+            console.log(args.token);
+
+            console.log("login ok 1!");
+
+
+            // this.loginService.logoff();
+            this.router.navigate(["/groceries"]);
+            // this.router.navigate(["/"]);
+            // this.gotonextpage();
+
+            appSettings.setString("UserData", JSON.stringify(args));
+        }, (error) => {
+            alert(error);
+        });
+
+        console.log("login ok 2!");
+        // this.gotonextpage();
+
+
+    }
+
+    gotonextpage() {
+    this.router.navigate(["/groceries"]);
+    // this.router.navigate(["/"]);
+
+  }
+
+  test1(){
+
+  }
+  test2(){
+    this.test1();
+    this.gotonextpage();
+  }
+}
