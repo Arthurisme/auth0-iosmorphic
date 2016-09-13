02da7cd7f884db6be142fc1c22599c2e13f2e59b
Arthurisme <is@live.ca>
2016-09-13 17:58:21 -0400

    use different name for each image by add prefix of time

--- Spring-backend/Spring-Auth0-Iosmorphic-Starter/src/main/java/com/auth0/example/controller/PhotoResource.java
+++ Spring-backend/Spring-Auth0-Iosmorphic-Starter/src/main/java/com/auth0/example/controller/PhotoResource.java
@@ -7,6 +7,8 @@ import com.auth0.example.service.PhotoService;
 import com.auth0.example.service.UserService;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +25,19 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 
 @RestController
 @RequestMapping("/rest")
 public class PhotoResource {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
-	
+
+
 	private String imageName;
 
 
@@ -41,17 +49,29 @@ public class PhotoResource {
 
 	@Autowired
 	protected UsernameService usernameService;
-	
+
+	//upload is before add, so imageName changed by upload can be consumed by add.
 	@RequestMapping(value="/photo/upload", method= RequestMethod.POST)
 	public String upload(HttpServletResponse response, HttpServletRequest request){
 		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
 		Iterator<String> it = multipartRequest.getFileNames();
 		MultipartFile multipartFile = multipartRequest.getFile(it.next());
+
+
+		//Move this as a service:
+			DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+			//get current date time with Date()
+			Date date = new Date();
+			String currentTimeString=(dateFormat.format(date));
+		logger.info("currentTimeString 10: " + currentTimeString + "  ");
+
+		//End Move this as a service:
+
 		
 		String fileName = multipartFile.getOriginalFilename();
-		imageName =fileName;
+		imageName = currentTimeString +"_"+fileName;
 		
-		String path = new File("src/main/resources/static/images").getAbsolutePath()+"/"+fileName;
+		String path = new File("src/main/resources/static/images").getAbsolutePath()+"/"+imageName;
 	    System.out.println("test path before uploaded: "+path);
 
 		
